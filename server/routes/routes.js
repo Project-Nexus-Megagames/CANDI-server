257ead65356e2c928aa5e124167e17f542089234
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2020-07-23 02:07:26 -0700

    ADDED - Config and Prod middleware

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,3 +1,4 @@
+const { logger } = require("../middleware/log/winston"); // Import of winston for error logging
 const bodyParser = require('body-parser'); // Body Parser Middleware
 
 // Routes - Using Express
@@ -5,7 +6,7 @@ const home = require('../routes/public/home');
 
 // Route Function
 module.exports = function(app) {
-    console.log('Route Middleware Loaded...')
+    logger.info('Route Middleware Loaded...')
     app.use(bodyParser.json()); // Tells Express to use Body Parser for JSON
 
     app.use('/', home);
