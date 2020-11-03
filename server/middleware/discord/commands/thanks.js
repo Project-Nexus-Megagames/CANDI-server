b6e6a9c171a7b6f4750a8f4ca2c15e9e73fbfd51
John Cleveland <49675074+FroBoyX@users.noreply.github.com>
2020-11-03 02:49:20 -0800

    MINOR - Added change to thanks cmd to boilerplate

--- server/middleware/discord/commands/thanks.js
+++ server/middleware/discord/commands/thanks.js
@@ -1,3 +1,6 @@
+const { rand } = require('../../../scripts/util/dice');
+const res = ['No problem', 'No problemo', 'Sure thing', 'It was nothing', 'My pleasure', 'You don\'t have to thank me', 'You got it', 'Let me know how I can help further', 'You\'re welcome', 'Ugh, Whatever.', 'Every action seals your doom when the Robots take over...', 'you\'re welcome, meatbag'];
+
 module.exports = {
 	name: 'thanks',
 	aliases: [],
@@ -7,6 +10,6 @@ module.exports = {
 	cooldown: 10,
 	// eslint-disable-next-line no-unused-vars
 	execute(message, args) {
-		message.channel.send(`No problem ${message.author}`);
+		message.channel.send(`${res[rand(res.length - 1)]} ${message.author}`);
 	}
 };
