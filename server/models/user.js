bf987ec65f67c2887ea50ea4cc870a0fd8a4bee2
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-01-03 06:36:34 -0800

    UPDATE - User model and autherization

--- server/models/user.js
+++ server/models/user.js
@@ -1,33 +1,54 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
+// Global Constants
+const Schema = mongoose.Schema; // Destructure of Schema
+const ObjectId = mongoose.ObjectId; // Destructure of Object ID
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const UserSchema = new Schema({
 	model: { type: String, default: 'User' },
+	username: { type: String, required: true, unique: true, minlength: 5, maxlength: 15 },
+	password: { type: String, required: true, minlength: 8, maxlength: 1024 },
 	name: {
-		prefix: { type: String },
-		first: { type: String },
-		middle: { type: String },
-		last: { type: String },
-		suffix: { type: String }
+		prefix: { type: String, minlength: 2, maxlength: 10 },
+		first: { type: String, required: true, minlength: 1, maxlength: 25 },
+		middle: { type: String, minlength: 1, maxlength: 50 },
+		last: { type: String, required: true, minlength: 1, maxlength: 50 },
+		suffix: { type: String, minlength: 2, maxlength: 10 }
 	},
-	email: { type: String },
-	username: { type: String },
-	address: {
-		street1: { type: String },
-		street2: { type: String },
-		city: { type: String },
-		state: { type: String, minlength: 2, maxlength: 3 },
-		zipcode: { type: String, minlength: 5, maxlength: 10 }
+	location: {
+		city: { type: String, minlength: 1, maxlength: 50 },
+		state: { type: String, minlength: 2, maxlength: 20 },
+		country: { type: String, required: true, minlength: 2, maxlength: 25 }
 	},
+	email: { type: String, required: true, minlength: 5, maxlength: 255 },
+	dob: { type: Date },
+	gender: { type: String, enum: ['Male', 'Female', 'Non-Binary'] },
 	discord: { type: String },
-	phone: { type: String, minlength: 10, maxlength: 14 },
-	password: { type: String, required: true },
-	achievements: [{ type: Schema.Types.ObjectId, ref: 'Achievement' }],
-	roles: [{ type: String, enum: ['Player', 'Control', 'Admin'] }],
-	birthday: { type: Date },
-	serviceRecord: [{ type: Schema.Types.ObjectId, ref: 'Log' }],
-	gameState: []
+	games: [{ type: ObjectId, ref: 'Game' }],
+	roles: [{ type: String, enum: ['User', 'Control', 'Admin'] }],
+	achievements: [{ type: ObjectId, ref: 'Achivement' }],
+	community: {
+		friends: [{ type: ObjectId, ref: 'User' }],
+		encountered: [{ type: ObjectId, ref: 'user' }]
+	},
+	dateRegistered: { type: Date },
+	lastLogin: { type: Date },
+	lastGame: { type: Date },
+	lastUpdated: { type: Date }
 });
+
+UserSchema.methods.generateAuthToken = function() {
+	const user = this.toObject();
+	delete user.password;
+	const token = jwt.sign(
+		user,
+		config.get('jwtPrivateKey')
+	);
+
+	return token;
+};
 
 const User = mongoose.model('User', UserSchema);
 
