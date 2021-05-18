287da7145cb6ee9e2797da4b7bbac652a33007d5
John T. Cleveland <John.Cleveland@WestCoastMegagames.com>
2021-05-18 00:01:27 -0700

    MINOR - Changed history model

--- models/history.js
+++ models/history.js
@@ -2,16 +2,17 @@ const mongoose = require('mongoose'); // Mongo DB object modeling module
 
 // Global Constants
 const Schema = mongoose.Schema; // Destructure of Schema
-// const ObjectId = mongoose.ObjectId; // Destructure of Object ID
+const ObjectId = mongoose.ObjectId; // Destructure of Object ID
 
 const HistorySchema = new Schema({
 	model: { type: String, default: 'History' },
 	timeStamp: { type: Date },
 	docType: { type: String },
 	action: { type: String },
+	function: { type: String },
 	document: { type: Schema.Types.Mixed },
 	user: { type: String },
-	character: { type: String }
+	character: { type: ObjectId, ref: 'Character' }
 });
 
 const History = mongoose.model('History', HistorySchema);
