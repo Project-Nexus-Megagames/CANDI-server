7c7f90641806518cfcb07b4a3a2f11bfd4e6372e
SMcMann <smcmann42@gmail.com>
2021-09-15 00:43:28 -0700

    edit character model for GW

--- models/character.js
+++ models/character.js
@@ -6,6 +6,16 @@ const mongoose = require('mongoose'); // Mongo DB object modeling module
 const Schema = mongoose.Schema; // Destructure of Schema
 const ObjectId = mongoose.ObjectId; // Destructure of Object ID
 
+const MortalRelationshipSchema = new Schema({
+	with: { type: ObjectId, ref: 'Character' },
+	level: { type: String, enum: ['Neutral', 'Warm', 'Friendly', 'Bonded' ] }
+});
+
+const GodRelationshipSchema = new Schema({
+	with: { type: ObjectId, ref: 'Character' },
+	level: { type: String, enum: ['Neutral', 'Preferred', 'Favoured', 'Blessed' ] }
+});
+
 const CharacterSchema = new Schema({
 	model:  { type: String, default: 'Character' },
 	playerName: { type: String, minlength: 1, maxlength: 50, required: true },
@@ -15,20 +25,45 @@ const CharacterSchema = new Schema({
 	timeZone: { type: String, default: '???' },
 	characterName: { type: String, minlength: 2, maxlength: 50, required: true },
 	characterActualName: { type: String, maxlength: 50, default: 'None' },
-	tag: [{ type: String }],
+	tags: [{ type: String }],
 	username: { type: String, minlength: 2, maxlength: 50, required: true },
 	pronouns: { type: String },
 	control: { type: String, default: 'None' },
 	bio: { type: String },
 	standingOrders: { type: String },
-	assets: [{ type: ObjectId, ref: 'Asset' }], // change to asset ID
 	lentAssets: [{ type: ObjectId, ref: 'Asset' }], // change to asset ID
-	popSupport: { type: Number, default: 0 },
-	supporters: [{ type: String }],
+	Justice: { type: Number, default: 0 },
+	Trickery: { type: Number, default: 0 },
+	Balance: { type: Number, default: 0 },
+	Hedonism: { type: Number, default: 0 },
+	Bonding: { type: Number, default: 0 },
+	Arts: { type: Number, default: 0 },
+	Sporting: { type: Number, default: 0 },
+	Fabrication: { type: Number, default: 0 },
+	Scholarship: { type: Number, default: 0 },
+	Pugilism : { type: Number, default: 0 },
+	supporters: [{ type: String }], // legacy
 	effort: { type: Number, default: 2, min: 0, max: 6 },
 	// color: { type: String, default: 'ffffff' }
 });
 
 const Character = mongoose.model('Character', CharacterSchema);
 
-module.exports = { Character };
+const Mortal = Character.discriminator(
+	'Mortal',
+	new Schema({
+		type: { type: String, default: 'Mortal' },
+		relationships: [ MortalRelationshipSchema ]
+	})
+);
+
+const God = Character.discriminator(
+	'God',
+	new Schema({
+		type: { type: String, default: 'God' },
+		relationships: [ GodRelationshipSchema ]
+	})
+);
+
+
+module.exports = { Character, Mortal, God };
