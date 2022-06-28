8e12874e9494e36ab6225f230fb2121b010e45eb
SMcMann <smcmann42@gmail.com>
2022-06-28 16:06:28 -0700

    bu g fix

--- game/gamestate.js
+++ game/gamestate.js
@@ -121,7 +121,7 @@ async function nextRound() {
 
 		for (const character of await Character.find()) {
 			character.lentAssets = [];
-			character.effort = 2;
+			character.effort = 3;
 			character.injuries = character.injuries.filter((el) => (el.received + el.duration) > gamestate.round || el.permanent);
 			character.save();
 			console.log(`Restoring effort of ${character.characterName}`);
