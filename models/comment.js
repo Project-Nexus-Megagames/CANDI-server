f478f918e30345c45a8cde4fb33f48ed386f3214
Franzi <34717874+franziskas1981@users.noreply.github.com>
2022-08-19 00:28:40 +0200

    adding commentorProfileImage to comment

--- models/comment.js
+++ models/comment.js
@@ -8,7 +8,8 @@ const CommentSchema = new Schema({
 	type: { type: String, default: 'Comment', enum: ['Comment', 'Info'] },
 	status: { type: String, default: 'Temp-Hidden', enum: ['Public', 'Private', 'Temp-Hidden'] },
 	body: { type: String, required: true },
-	commentor: { type: String, required: true }
+	commentor: { type: String, required: true },
+	commentorProfileImage: { type: String }
 }, { timestamps: true });
 
 const Comment = mongoose.model('Comment', CommentSchema);
